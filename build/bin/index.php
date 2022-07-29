d379a928bd1e0778b940069fae41d34c713ef12d
Nima Samadi <nbr.smd@gmail.com>
2022-07-29 22:42:58 +0430

    UI updated

--- build/bin/index.php
+++ build/bin/index.php
@@ -1,28 +1,27 @@
+<!doctype html>
 <html>
 <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Emb Project Web Server</title>
 </head>
-<body>
-    <div>
-        <div style="width:40%; border:solid 1px black; margin-left:auto; margin-right:auto; padding:10px;">
-            <form method="GET" action="index.php">
-                Update image:
-                <input type="hidden" name="update" value="true">
-                <input type="submit">
-            </form>
-            <hr/>
-        </div>
+<body style="font-size:40px; text-align:center; margin:0; background-color: #FFF8DC">
+    <div class="row">
+        <div class="w-50 rounded" style="background-color:#007bff; margin-top: 20px; margin-left:auto; margin-right:auto; padding:10px; color: white;">Real-time Embedded Systems Course Project</div>
     </div>
-    <div>
-        <img src="./img.jpg">
+    <div class="row">
+        <div class="w-50 shadow rounded" style="width:40%; border:solid 1px white; margin-left:auto; margin-right:auto; padding:10px; background-color: white;">
+            <img class="w-75 rounded" src="./img.jpg" style="border: solid #007bff 2px;">
+            <form method="GET" action="index.php">
+                <input type="hidden" name="update" value="true">
+                <input class="btn btn-primary" style="font-size:18px" type="submit" value="Update image">
+            </form>
+        </div>
     </div>
     <?php
         switch($_SERVER["REQUEST_METHOD"]){
             case 'GET':
                 if (!empty($_GET)){
                     if ($_GET["update"] == "true"){
-                        print_r("Updating image...");
                     }
                     $res = $_GET;
                 }else{
@@ -31,5 +30,6 @@
             break;
         }
     ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 </html>
